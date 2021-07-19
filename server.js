f93d42f91f8637e21761b8fb06c62b2da3c3d929
JerryLin1 <jerreelynn@gmail.com>
2021-07-19 13:17:38 -0400

    lobby start

---
+++ server.js
@@ -0,0 +1,55 @@
+// ============== Magic =================
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+// ======================================
+
+const port = process.env.PORT || 6567;
+server.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+});
+var rooms = [];
+
+// Whenever a client connects
+io.on('connection', socket => {
+    console.log(`ID: ${socket.id} has joined.`);
+    socket.on('disconnect', () => {
+        console.log(`ID: ${socket.id} has disconnected.`);
+    });
+
+    // When client creates lobby
+    socket.on("createRoom", () => {
+        let gid = RandomId(8);
+        // Redirect client to new URL
+        socket.emit("redirect", gid);
+        rooms.push(gid);
+        console.log(`Room ${gid} created.`);
+    });
+    
+    // Joins client to room
+    socket.on("joinRoom", id => {
+        // If room exists, join client to room
+        if (rooms.includes(id)) {
+            socket.join(id);
+        }
+        // Else redirect them back to home
+        else {
+            socket.emit("redirect", "/");
+        }
+    })
+    //TODO: Remove ID from rooms when empty
+});
+
+// Server debug messages
+setInterval(() => {
+    console.log(`${io.engine.clientsCount} clients.`);
+    console.log(rooms);
+}, 10000)
+
+// Room id generator
+function RandomId(length) {
+    return Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, length);
+}
--- src/index.js
+++ src/index.js
@@ -1,16 +1,77 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { io } from "socket.io-client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
+var socket;
+function App() {
+  socket = io();
 
+  // At start, attempt to join the room ID from the URL
+  let roomId = window.location.pathname + window.location.search;
+  roomId = roomId.substring(1);
+  if (roomId.length > 1) {
+    JoinRoom(roomId);
+  }
+
+  // For debug
+  socket.on("log", (msg) => {
+    console.log(msg);
+  });
+
+  // Redirect URL (e.g. when client creates room)
+  socket.on("redirect", (id) => {
+    Redirect(id);
+  });
+
+  return (
+    <div className="App">
+      ANGRY RHYME TOWN
+      <BtnCreateRoom />
+      <FormJoinRoom />
+    </div>
+  );
+}
+// Create the room, then redirect URL to that room
+function CreateRoom() {
+  socket.emit("createRoom");
+}
+function JoinRoom(roomId) {
+  socket.emit("joinRoom", roomId);
+}
+function BtnCreateRoom() {
+  return (
+    <div>
+      <button onClick={CreateRoom}>Create lobby</button>
+    </div>
+  );
+}
+
+function FormJoinRoom() {
+  return (
+    <div>
+      <input id="inputRoomID" type="text" />
+      <button
+        onClick={() => {
+          console.log(document.getElementById("inputRoomID").value);
+          Redirect(document.getElementById("inputRoomID").value);
+        }}
+      >
+        Join Room
+      </button>
+    </div>
+  );
+}
+function Redirect(id) {
+  window.location.href = id;
+}
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
