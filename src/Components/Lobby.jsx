a05943453c49f11b3bd357a92a30d6747d6f5d6c
JerryLin1 <jerreelynn@gmail.com>
2021-07-19 16:36:32 -0400

    director

---
+++ src/Components/Lobby.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import $ from "jquery";
+import cclient from "../client.js";
+
+export default class Lobby extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>TODO: Lobby</div>
+      </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import Home from "./Components/Home"
+import Home from "./Components/Home";
+import Lobby from "./Components/Lobby";
 import reportWebVitals from "./reportWebVitals";
 
-
 ReactDOM.render(
-  <React.StrictMode>
-    <Home/>
-  </React.StrictMode>,
+  director(),
   document.getElementById("root")
 );
 
-
+function director() {
+  if (window.location.pathname + window.location.search === "/") {
+    return <Home />;
+  } else {
+    return <Lobby />;
+  }
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
