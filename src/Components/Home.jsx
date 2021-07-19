2d549c9665a2cf31d870f6834c2e5a1ce1df4052
p-han-tom <hantom0127@gmail.com>
2021-07-19 15:52:45 -0400

    Moved Home functions to index.js

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,40 +2,26 @@ import React from "react";
 import io from "socket.io-client";
 import $ from "jquery";
 
-const socket = io();
 
 export default class Home extends React.Component {
-  componentDidMount() {
-    // At start, attempt to join the room ID from the URL
-    let roomId = window.location.pathname + window.location.search;
-    roomId = roomId.substring(1);
-    if (roomId.length > 1) {
-      this.joinRoom(roomId);
-    }
 
-    // For debug
-    socket.on("log", (msg) => {
-      console.log(msg);
-    });
-
-    // Redirect URL (e.g. when client creates room)
-    socket.on("redirect", (id) => {
-      this.redirect(id);
-    });
+  constructor(props) {
+    super(props);
   }
+  
 
   render() {
     return (
       <div className="App">
         ANGRY RHYME TOWN
         <div>
-          <button onClick={this.createRoom}>Create lobby</button>
+          <button onClick={this.props.createRoom}>Create lobby</button>
         </div>
         <div>
           <input id="inputRoomID" type="text" />
           <button
             onClick={() => {
-              this.redirect($("#inputRoomID").val());
+              this.props.redirect($("#inputRoomID").val());
             }}
           >
             Join Room
@@ -44,16 +30,6 @@ export default class Home extends React.Component {
       </div>
     );
   }
-  createRoom = () => {
-    socket.emit("createRoom");
-  };
-  joinRoom = (roomId) => {
-    socket.emit("joinRoom", roomId);
-  };
 
-  redirect = (id) => {
-    window.location.href = id;
-  };
 }
 
-// Create the room, then redirect URL to that room
--- src/index.js
+++ src/index.js
@@ -1,15 +1,53 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import io from "socket.io-client";
 import Home from "./Components/Home"
 import reportWebVitals from "./reportWebVitals";
 
+const socket = io();
+
+// At start, attempt to join the room ID from the URL
+let roomId = window.location.pathname + window.location.search;
+roomId = roomId.substring(1);
+if (roomId.length > 1) {
+  joinRoom(roomId);
+}
+
+function createRoom() {
+  socket.emit("createRoom");
+}
+
+function joinRoom(roomId) {
+  socket.emit("joinRoom", roomId);
+}
+
+function redirect(id) {
+  window.location.href = id;
+}
+
+// For debug
+socket.on("log", (msg) => {
+  console.log(msg);
+});
+
+// Redirect URL (e.g. when client creates room)
+socket.on("redirect", (id) => {
+  redirect(id);
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+    <Home socket = {socket}
+    createRoom = {createRoom}
+    joinRoom = {joinRoom}
+    redirect = {redirect}
+    />
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
