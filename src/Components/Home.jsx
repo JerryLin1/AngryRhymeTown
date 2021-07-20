073e410a1cdb4da3e96a1fbbc10eac70f456ad85
Roseak-lin <roseak.lin@gmail.com>
2021-07-19 20:21:33 -0400

    Re organized UI

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,6 +3,7 @@ import $ from "jquery";
 import cclient from "../client.js";
 import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 
 import home from "./Home.module.css";
 
@@ -13,29 +14,36 @@ export default class Home extends React.Component {
     return (
       <div className={`${home.App}`}>
         <h1 id="title">ANGRY RHYME TOWN</h1>
-        <Col>
-          <Button
-            variant="outline-dark"
-            id={`${home.createLobby}`}
-            onClick={client.createRoom}
-          >
-            Create lobby
-          </Button>
-          <input
-            placeholder="Lobby Code"
-            type="text"
-            id={`${home.inputRoomID}`}
-          />
-          <Button
-            variant="outline-dark"
-            onClick={() => {
-              client.redirect($(`#${home.inputRoomID}`).val());
-            }}
-            id={`${home.joinRoom}`}
-          >
-            Join Room
-          </Button>
-        </Col>
+        
+        <Row>
+          <Col>PLACEHOLDER</Col>
+        </Row>
+
+        <Row>
+          <Col>
+            <Button
+              variant="outline-dark"
+              id={`${home.createLobby}`}
+              onClick={client.createRoom}
+            >
+              Create lobby
+            </Button>
+            <input
+              placeholder="Lobby Code"
+              type="text"
+              id={`${home.inputRoomID}`}
+            />
+            <Button
+              variant="outline-dark"
+              onClick={() => {
+                client.redirect($(`#${home.inputRoomID}`).val());
+              }}
+              id={`${home.joinRoom}`}
+            >
+              Join Room
+            </Button>
+          </Col>
+        </Row>
       </div>
     );
   }
--- src/Components/Lobby.jsx
+++ src/Components/Lobby.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import $ from "jquery";
 import cclient from "../client.js";
+import Row from "react-bootstrap/esm/Row";
+import Col from "react-bootstrap/esm/Col";
+
+import lobby from "./Lobby.module.css"
+
+const client = new cclient();
 
 export default class Lobby extends React.Component {
-  render() {
+   render() {
     return (
-      <div>
-        <div>TODO: Lobby</div>
+      <div className={`${lobby.lobby}`}>
+        <Row>
+          <Col>
+            <h3>Room code: (SOCKET ID)</h3>
+          </Col>
+        </Row>
       </div>
     );
   }
