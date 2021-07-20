bb0d0b7d075d1d0cfe04f88bfd793e498f7a35f7
Roseak-lin <roseak.lin@gmail.com>
2021-07-19 20:52:57 -0400

    pull

--- src/Components/Lobby.jsx
+++ src/Components/Lobby.jsx
@@ -4,17 +4,23 @@ import cclient from "../client.js";
 import Row from "react-bootstrap/esm/Row";
 import Col from "react-bootstrap/esm/Col";
 
-import lobby from "./Lobby.module.css"
+import lobby from "./Lobby.module.css";
 
 const client = new cclient();
 
 export default class Lobby extends React.Component {
-   render() {
+  render() {
     return (
       <div className={`${lobby.lobby}`}>
         <Row>
           <Col>
             <h3>Room code: (SOCKET ID)</h3>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col xs="5" id={`${lobby.left}`}>
+            Players:
           </Col>
         </Row>
       </div>
