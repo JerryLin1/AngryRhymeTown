16772d1a75c6bcb433d689480fc97a5525a7b7b3
Roseak-lin <roseak.lin@gmail.com>
2021-07-21 10:15:43 -0400

    fixed alignment issues and chat

--- src/Components/Lobby.jsx
+++ src/Components/Lobby.jsx
@@ -19,43 +19,60 @@ export default class Lobby extends React.Component {
     this.client = props.client;
   }
 
-
+  // add handler to the chat input field when the page loads
+  componentDidMount() {
+    $(`#${lobby.chatInput}`).on("keydown", (e) => {
+      if (e.code == "Enter") {
+        this.client.sendMessage($(`#${lobby.chatInput}`).val());
+        $(`#${lobby.chatInput}`).val("");
+      }
+    });
+  }
 
   render() {
     return (
       <div className={`${lobby.lobby}`}>
-        <Row>
+        {/* First row that displays the room code */}
+        <Row id={`${lobby.row_1}`}>
           <Col>
-            <h3 style={{ display: "inline-block" }}>Room Code:&ensp;</h3>
-            <Form.Control
-              as="textarea"
-              style={{ height: "1em", width: "10em", display: "inline" }}
-              id="roomCode"
-            >
-              {roomId}
-            </Form.Control>
-            <OverlayTrigger
-              placement="right"
-              overlay={(props) => (
-                <Tooltip id={`${lobby.tooltip}`} {...props}>
-                  Copy Code
-                </Tooltip>
-              )}
-            >
-              <Clipboard
-                id={`${lobby.cb}`}
-                onMouseDown={() => {
-                  $("#roomCode").select();
-                  document.execCommand("copy");
-                  $(".tooltip-inner").text("Copied!");
-                }}
+            <Form.Group>
+              <Form.Label column>Room Code: &ensp;</Form.Label>
+              <Form.Control
+                id={`${lobby.roomCode}`}
+                value={roomId}
+                readOnly
+                plaintext
               />
-            </OverlayTrigger>
-            <input
+              <OverlayTrigger
+                placement="right"
+                overlay={(props) => (
+                  <Tooltip id={`${lobby.tooltip}`} {...props}>
+                    Copy Code
+                  </Tooltip>
+                )}
+              >
+                <Clipboard
+                  id={`${lobby.cb}`}
+                  onMouseDown={() => {
+                    $("#roomCode").select();
+                    document.execCommand("copy");
+                    $(".tooltip-inner").text("Copied!");
+                  }}
+                />
+              </OverlayTrigger>
+            </Form.Group>
+          </Col>
+        </Row>
+
+        {/* Second row that displays the nickname changer */}
+        <Row id={`${lobby.row_2}`}>
+          <Col xs="auto">
+            <Form.Control
               placeholder="Nickname"
-              type="text"
               id={`${lobby.inputNickname}`}
             />
+          </Col>
+          <Col xs="auto">
             <Button
               variant="outline-dark"
               onClick={() => {
@@ -68,52 +85,43 @@ export default class Lobby extends React.Component {
           </Col>
         </Row>
 
-
-
-        <Row>
+        {/* Third row that displays the players on the left and the room chat on the right */}
+        <Row id={`${lobby.row_3}`}>
+          {/* Player list */}
           <Col xs="6">
             <Card style={{ height: "48em" }}>
               <Card.Header style={{ fontSize: "2em " }}>
                 Player List
               </Card.Header>
-              <Card.Body id="lobbyList">
-              </Card.Body>
+              <Card.Body id="lobbyList"></Card.Body>
             </Card>
           </Col>
+
+          {/* Lobby Chat */}
           <Col>
             <Card style={{ height: "24em" }}>
-              <Card.Header style={{ fontSize: "2em " }}>
-                Chat
-              </Card.Header>
-              <Card.Body id = "chat">
-                
-                <div id={`${lobby.sendbar}`}>
-                  <input
-                    placeholder="Type a message..."
-                    type="text"
-                    id={`${lobby.chatInput}`}
-                  />
-                  <Button
-                    variant="outline-dark"
-                    onClick = {() => {
-                      this.client.sendMessage($(`#${lobby.chatInput}`).val());
-                    }}
-                    id={`${lobby.chatEnter}`}
-                  >
-                    Send Message
-                  </Button>
-
-                </div>
-
-              </Card.Body>
+              <Card.Header style={{ fontSize: "2em" }}>Chat</Card.Header>
+              <Card.Body id="chat" style={{ overflowY: "scroll" }}></Card.Body>
             </Card>
-
-
-
+            <div id={`${lobby.sendbar}`}>
+              <input
+                placeholder="Type a message..."
+                type="text"
+                id={`${lobby.chatInput}`}
+              />
+              <Button
+                variant="outline-dark"
+                onClick={() => {
+                  this.client.sendMessage($(`#${lobby.chatInput}`).val());
+                  $(`#${lobby.chatInput}`).val("");
+                }}
+                id={`${lobby.chatEnter}`}
+              >
+                Send Message
+              </Button>
+            </div>
           </Col>
         </Row>
-
-
       </div>
     );
   }
