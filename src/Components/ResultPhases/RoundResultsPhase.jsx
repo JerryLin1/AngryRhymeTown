857504b3970749862e307e841eed33afcb94ea26
Roseak-lin <roseak.lin@gmail.com>
2021-07-28 14:34:01 -0400

    split all result pages into separete files

--- src/Components/ResultPhases/RoundResultsPhase.jsx
+++ src/Components/ResultPhases/RoundResultsPhase.jsx
@@ -1,31 +1,40 @@
 import React from "react";
 import { Row, Col } from "react-bootstrap";
+import { Award } from "react-bootstrap-icons";
 import game from "../Game.module.css";
 
 export default class RoundResultsPhase extends React.Component {
   constructor(props) {
     super(props);
   }
+
   render() {
     return (
       <div>
         <div className={`${game.header}`}>
-          Here are the results for round {"ROUND"}!
+          Here are the results for round {"**round number**"}!
         </div>
-        
+
         <br />
 
         <div id={`${game.rapperList}`}>
           <Row>
-            <Col xs={{ offset: 1, span: 8 }}>1. linrose362</Col>
+            <Col xs="8">
+              1. linrose362 <Award style={{ color: "#d4af37" }} />
+            </Col>
             <Col>100 points</Col>
           </Row>
           <Row>
-            <Col xs={{ offset: 1, span: 8 }}>2. linjerr492</Col>
+            <Col xs="8">
+              2. linjerr492 <Award style={{ color: "#C0C0C0" }} />
+            </Col>
             <Col>75 points</Col>
           </Row>
           <Row>
-            <Col xs={{ offset: 1, span: 8 }}>3. p.han.tom</Col>
+            <Col xs="8">
+              3. p.han.tom <Award style={{ color: "#cd7f32 " }} />
+            </Col>
+
             <Col>50 points</Col>
           </Row>
         </div>
