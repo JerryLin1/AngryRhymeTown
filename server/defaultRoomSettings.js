7699e5665721f70b25bfb5f61718b1b691bf2d78
p-han-tom <hantom0127@gmail.com>
2021-07-28 14:40:00 -0400

    scuff playthrough

--- server/defaultRoomSettings.js
+++ server/defaultRoomSettings.js
@@ -1,8 +1,8 @@
 module.exports = DEFAULT_ROOM_SETTINGS = {
     numberOfRounds: 1,
     pairingTime: 3000,
-    writingTime: 5000,
-    votingTime: 5000,
+    writingTime: 60000,
+    votingTime: 15000,
     votingResultsTime: 5000,
     roundResultsTime: 5000
 }
