9c6be6884b2bfeff45a9c178722fdb23cce6586e
JerryLin1 <jerreelynn@gmail.com>
2021-07-29 15:48:22 -0400

    tts

--- server/defaultRoomSettings.js
+++ server/defaultRoomSettings.js
@@ -1,8 +1,8 @@
 module.exports = DEFAULT_ROOM_SETTINGS = {
     numberOfRounds: 3,
-    pairingTime: 1000,
-    writingTime: 1000,
+    pairingTime: 3000,
+    writingTime: 60000,
     votingTime: 3000,
-    votingResultsTime: 5000,
+    votingResultsTime: 3000,
     roundResultsTime: 5000
 }
