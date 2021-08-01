0208508cc42a3159f81ab228e4dce9b7f0e6fd01
p-han-tom <hantom0127@gmail.com>
2021-08-01 16:26:18 -0400

    speech from server

--- src/Components/GamePhases/RappingPhase.jsx
+++ src/Components/GamePhases/RappingPhase.jsx
@@ -6,41 +6,60 @@ import game from "../Game.module.css";
 export default class RappingPhase extends React.Component {
   constructor(props) {
     super(props);
+    this.client = this.props.client;
+    this.socket = this.props.client.socket;
 
     this.state = {
       backgroundImgs: [
         "https://mediacloud.kiplinger.com/image/private/s--o_dOiB7J--/v1599666964/Investing/small-town-millionaires-2020.jpg",
       ],
-      bars: [],
+      currentBattle: 0,
+      matchup: [
+        { nickname: "", bars: ["", "", "", ""] },
+        { nickname: "", bars: ["", "", "", ""] },
+      ],
       barDivs: [],
+      ssu: tts.newSSU()
     };
-    this.state.bars = [
-      "Hey my name is Jerry Lin",
-      "Everyday I only win",
-      "Every time I get that dub",
-      "I take a bath inside my tub",
-    ];
+
     // TODO: socket.on for incoming matchup. readBars for both raps, with a delay in between.
-    this.readBars();
+    this.socket.on("receiveBattleRapping", matchup => {
+      this.setState({ currentBattle: 0 });
+      this.setState({ barDivs: [] });
+      this.setState({ matchup: matchup });
+
+      this.readBars();
+    })
+
   }
+
   async readBars() {
-    this.state.ssu = tts.newSSU();
-    for (let bar of this.state.bars) {
+    for (let bar of this.state.matchup[this.state.currentBattle].bars) {
       this.state.barDivs.push(<div>{bar}</div>);
       this.forceUpdate();
       if (!document.hidden)
         await tts.speak(this.state.ssu, bar);
     }
+
+    if (this.state.currentBattle < 1) {
+      this.setState({ currentBattle: this.state.currentBattle + 1 });
+      this.setState({ barDivs: [] });
+      this.readBars();
+    } else {
+
+      this.socket.emit("finishedListenin");
+
+    }
+    return;
+
   }
-  // It's supposed to play each rap and switch after the tts is over but idk how to figure out when tts is over
   render() {
     return (
       <div
         className={`${game.rapPhase}`}
         style={{
-          background: `url('${
-            this.state.backgroundImgs[Math.floor(Math.random())]
-          }')`,
+          background: `url('${this.state.backgroundImgs[Math.floor(Math.random())]
+            }')`,
           backgroundRepeat: "no-repeat",
           backgroundPosition: "center",
         }}
@@ -49,7 +68,7 @@ export default class RappingPhase extends React.Component {
           className={`${game.header}`}
           style={{ backdropFilter: "blur(0.1em)" }}
         >
-          Let's hear those bars!
+          Let's hear those bars from {this.state.matchup[this.state.currentBattle].nickname}!
         </div>
         <br />
         <Row>
