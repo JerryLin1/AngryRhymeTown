e0b065ff318742e52de8accaebc8e18eed93d2f4
JerryLin1 <jerreelynn@gmail.com>
2021-08-11 13:08:24 -0400

    update avatar function

--- src/Components/Avatar/avatarFunctions.js
+++ src/Components/Avatar/avatarFunctions.js
@@ -4,6 +4,7 @@ export function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
 }
 export function isValidComponent(num, numCom) {
+    if (num === undefined || num === null) return false;
     if (num >= 0 && num < numCom) return true;
     else return false;
 }
